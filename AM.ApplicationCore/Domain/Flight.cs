8c1063e544ed2947757355ab85082a632a5765d9
jebali.hassen <hdjebali92@gmail.com>
2025-09-22 14:39:15 +0100

    2nd commit

---
+++ AM.ApplicationCore/Domain/Flight.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Flight
+    {
+
+        public string Destinaton { get; set; }
+
+        public int MyProperty { get; set; }
+        public DateTime Deparature { get; set; }
+
+        public DateTime FlightDate { get; set; }
+        public int FlightId { get; set; }
+        public DateTime EffectiveArrival { get; set; }
+        public int EstimatedDuration { get; set; }
+
+        public  ICollection<Passenger> Passengers { get; set;}
+        public  Plane Plane { get; set; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Passenger
+    {
+        public DateTime BirthDate { get; set; }
+        public int PassportNumber { get; set; }
+        public string EmailAdress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int TelNumber {  get; set; }
+
+        public ICollection<Flight> Flights { get; set; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
